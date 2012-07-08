71374cd12f9950f18a168fb52cfb045b75130c03
guiott <guiott@gmail.com>
2012-07-08 08:09:44 +0000

    Early development version. Only test procedures have been developed.

--- lowlevelsupervisor.ino
+++ lowlevelsupervisor.ino
@@ -1,10 +1,10 @@
 /* ////////////////////////////////////////////////////////////////////////////
-** File:      LinoLLS.ino
+** File:      LowLevelSupervisor.ino
 */                                  
  unsigned char  Ver[] = "LinoLLS     0.8.0 Guiott 07-12"; // 30+1 char
 
 /**
-* \mainpage LinoLLS.ino
+* \mainpage LowLevelSupervisor.ino
 * \author    Guido Ottaviani-->guido@guiott.com<--
 * \version 0.8.0
 * \date 07/12
@@ -14,18 +14,18 @@
 \copyright 2012 Guido Ottaviani
 guido@guiott.com
 
-    LinoLLS is free software: you can redistribute it and/or modify
+    LowLevelSupervisor is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    LinoLLS is distributed in the hope that it will be useful,
+    LowLevelSupervisor is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with LinoLLS.  If not, see <http://www.gnu.org/licenses/>.
+    along with LowLevelSupervisor.  If not, see <http://www.gnu.org/licenses/>.
 
 -------------------------------------------------------------------------------
 */
